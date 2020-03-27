6f8f28ec6b6587aa448b0750771082decbe69d6d
liutao <271297773@qq.com>
2020-03-27 09:11:35 +0800

    创建路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,42 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList'
+import EventCreate from '../views/EventCreate'
+import EventShow from '../views/EventShow'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   }
-];
+  // {
+  //   path: '/about',
+  //   redirect: { name: 'About' }
+  // }
+]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
-});
+})
 
-export default router;
+export default router
